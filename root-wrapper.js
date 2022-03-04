5b09a65d5e3d16122b8f4b1e779c6b34f931ba23
russellotniel <russellotniel@gmail.com>
2022-03-04 08:41:56 +0700

    navbar, footer, global css

--- root-wrapper.js
+++ root-wrapper.js
@@ -8,6 +8,40 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
 }
 
+ul {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+h1, h2, h3, h4 {
+  text-transform: capitalize;
+  font-weight: 400;
+  margin-bottom: 0.75rem;
+}
+
+h1 {
+  font-size: 2rem;
+}
+
+h2 {
+  font-size: 1.5rem;
+}
+
+h3 {
+  font-size: 1.25rem;
+}
+
+h4 {
+  font-size: 0.875rem;
+}
+
+@media screen and (min-width: 800px){
+  
+}
+
 .page {
   min-height: calc(100vh - (6rem + 4rem));
 }
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
 import Offcanvas from "react-bootstrap/Offcanvas"
+import styled from "styled-components"
 import { Link } from "gatsby"
 import { GiHamburgerMenu } from "react-icons/gi"
 
@@ -11,15 +12,17 @@ const CustomNavbar = () => {
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
   return (
-    <>
-      <Navbar>
+    <Wrapper>
+      <Navbar bg="dark">
         <Container>
           <Navbar.Brand>
-            <Link to="/">M.R Studios</Link>
+            <Link to="/" className="link">
+              <h2>M.R Studios</h2>
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <GiHamburgerMenu onClick={handleShow} />
+            <GiHamburgerMenu onClick={handleShow} className="hamburger" />
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -28,12 +31,40 @@ const CustomNavbar = () => {
         <Offcanvas.Header closeButton>
           <h1>Menu</h1>
         </Offcanvas.Header>
-        <h2>About</h2>
-        <h2>Product</h2>
-        <h2>Contact</h2>
+        <Offcanvas.Body>
+          <Link to="/about">
+            <h2>About</h2>
+          </Link>
+
+          <Link to="/product">
+            <h2>Product</h2>
+          </Link>
+
+          <Link to="/contact">
+            <h2>Contact</h2>
+          </Link>
+        </Offcanvas.Body>
       </Offcanvas>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.nav`
+  margin-bottom: 1rem;
+
+  .link {
+    color: whitesmoke;
+    text-decoration: none;
+  }
+
+  .hamburger {
+    font-size: 2rem;
+    color: white;
+  }
+
+  .hamburger:hover {
+    cursor: pointer;
+  }
+`
 
 export default CustomNavbar
