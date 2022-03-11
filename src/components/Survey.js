d6c2057c2c3b8d12ddfbd9f8108b0efaedc24287
russellotniel <russellotniel@gmail.com>
2022-03-11 12:24:10 +0700

    redesign survey

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -70,7 +70,7 @@ const Survey = () => {
       <Container>
         {loading && <h2 className="text-center py-5">Loading</h2>}
         {!loading && (
-          <List className="py-5">
+          <List className="py-4">
             <h2>Which brand do you prefer?</h2>
             <div className="line"></div>
             <ul className="votes">
@@ -82,11 +82,11 @@ const Survey = () => {
 
                 return (
                   <li key={id}>
-                    <h3>{name["en-US"]}</h3>
-                    <h3>{votes["en-US"]}</h3>
+                    <h3 className="pb-2">{name["en-US"]}</h3>
                     <Button variant="outline-dark" onClick={() => addVote(id)}>
                       Vote
                     </Button>
+                    <h6 className="pt-3">{votes["en-US"]} votes</h6>
                   </li>
                 )
               })}
@@ -108,6 +108,14 @@ const List = styled.div`
     font-weight: 600;
   }
 
+  h3 {
+    font-weight: 500;
+  }
+
+  button {
+    border: 1px solid;
+  }
+
   .line {
     border-bottom: 2px solid gray;
     width: 10%;
@@ -127,9 +135,11 @@ const List = styled.div`
 
   .votes li {
     margin: 1rem 0;
+    padding: 1rem;
+    border: 1px solid black;
   }
 
-  @media (min-width: 800px) {
+  @media (min-width: 1100px) {
     .line {
       width: 5%;
     }
@@ -138,81 +148,11 @@ const List = styled.div`
       flex-direction: row;
       justify-content: space-evenly;
     }
+
+    .votes li {
+      width: 20vw;
+    }
   }
 `
 
 export default Survey
-
-// client
-//   .getSpace("qa5s4zuftbb3")
-//   .then(space => space.getEnvironment("master"))
-//   .then(environment => environment.getEntry("7tYvL3dl0xTQcD4gAKDIbg"))
-//   .then(entry => {
-//     entry.fields.votes["en-US"] = entry.fields.votes["en-US"] + 1
-//     return entry.update()
-//   })
-//   .then(entry => console.log(`Entry ${entry.sys.id} votes updated with + 1.`))
-//   .catch(console.error)
-
-// await client
-//   .getSpace("qa5s4zuftbb3")
-//   .then(space => space.getEnvironment("master"))
-//   .then(environment => environment.getEntry(id))
-//   .then(entry => {
-//     return entry.update(tempItems)
-//   })
-//   .then(entry =>
-//     console.log(`Entry ${entry.fields.name["en-US"]} updated.`)
-//   )
-//   .catch(console.error)
-
-// await client
-//   .getSpace("qa5s4zuftbb3")
-//   .then(space => space.getEnvironment("master"))
-//   .then(environment => environment.getEntry(id))
-//   .then(entry => {
-//     entry.fields.votes["en-US"] += 1
-//     return entry.update()
-//   })
-//   .then(entry =>
-//     console.log(`Entry ${entry.fields.name["en-US"]} updated.`)
-//   )
-//   .catch(console.error)
-
-// await client
-//   .getSpace("qa5s4zuftbb3")
-//   .then(space => space.getEnvironment("master"))
-//   .then(environment => environment.getEntry(id))
-//   .then(entry => {
-//     const newItems = {
-//       id: entry.sys.id,
-//       fields: entry.fields,
-//     }
-//     setItems(newItems)
-//     setLoading(false)
-//     return entry.update(tempItems)
-//   })
-//   .then(entry =>
-//     console.log(`Entry ${entry.fields.name["en-US"]} updated.`)
-//   )
-//   .catch(console.error)
-
-// console.log(records)
-
-// const newItems = records.items.map(record => {
-//   const id = record.sys.id
-//   const fields = record.fields
-//   return { id, fields }
-// })
-// setItems(newItems)
-// setLoading(false)
-
-// const tempItems = [...items].map(item => {
-//   if (item.id === id) {
-//     let { id, fields } = item
-//     fields = { ...fields, votes: fields.votes["en-US"] + 1 }
-//     return { id, fields }
-//   } else {
-//     return item
-//   }
-// })
