c5b33f50b6769eb27521fc72038df22f75fcc8ff
russellotniel <russellotniel@gmail.com>
2022-03-15 11:49:50 +0700

    ilangin lazy and suspense, cmn loadable survey

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,31 @@
 import React, { lazy, Suspense } from "react"
 import loadable from "@loadable/component"
-const About = lazy(() => import("../components/About"))
-const Hero = lazy(() => import("../components/Hero"))
-const Layout = lazy(() => import("../components/Layout"))
-const Location = lazy(() => import("../components/Location"))
-const Contact = lazy(() => import("../components/Contact"))
-const SEO = lazy(() => import("../components/SEO"))
+import About from "../components/About"
+import Hero from "../components/Hero"
+import Layout from "../components/Layout"
+import Location from "../components/Location"
+import Contact from "../components/Contact"
+import SEO from "../components/SEO"
+// const About = lazy(() => import("../components/About"))
+// const Hero = lazy(() => import("../components/Hero"))
+// const Layout = lazy(() => import("../components/Layout"))
+// const Location = lazy(() => import("../components/Location"))
+// const Contact = lazy(() => import("../components/Contact"))
+// const SEO = lazy(() => import("../components/SEO"))
 const Survey = loadable(() => import("../components/Survey"))
 
-const renderLoader = () => <p></p>
+// const renderLoader = () => <p></p>
 
 const HomePage = () => {
   return (
-    <Suspense fallback={renderLoader()}>
-      <Layout>
-        <SEO title="Home" />
-        <Hero />
-        <About />
-        <Location />
-        <Survey />
-        <Contact />
-      </Layout>
-    </Suspense>
+    <Layout>
+      <SEO title="Home" />
+      <Hero />
+      <About />
+      <Location />
+      <Survey />
+      <Contact />
+    </Layout>
   )
 }
 
